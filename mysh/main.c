47d8e06fac0b589324a028bf94e5a9a6a54cf277
hal-bo <takano.kakkokari@gmail.com>
2022-06-20 19:01:50 +0900

    finish

--- mysh/main.c
+++ mysh/main.c
@@ -25,7 +25,7 @@ int main()
     while (1) {
         printf("$ ");
         struct command *cmd = getcommand();
-        print_command(cmd);
+        //print_command(cmd);
         // printf("--------------------\n");
         exec_command(cmd);
         //printf("--------------------\n");
